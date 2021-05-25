01f70762ea0a8613c917f18189487459abeea048
DallenPyrah <DallenPyrah@truckstop.com>
2021-05-25 12:54:42 -0600

    Added in local db and configured the home controller

--- OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -12,12 +12,13 @@ namespace OdeToFood.Data.Services
         {
             restaurants = new List<Restaurant>
             {
-                new Restaurant { Id = 1, Name = "Dallens Indian Shack", Cuisine = CuisineType.Indian },
-                new Restaurant { Id = 2, Name = "Tims French Cafe", Cuisine = CuisineType.French },
-                new Restaurant { Id = 3, Name = "Mikes Italian Pasta", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 1, Name = "Dallens Indian Shack", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 2, Name = "Tims French Cafe", Cuisine = CuisineType.French},
+                new Restaurant { Id = 3, Name = "Mikes Italian Pasta", Cuisine = CuisineType.Italian},
                 new Restaurant { Id = 4, Name = "Dallens None Shack", Cuisine = CuisineType.None }
             };
         }
+
         public IEnumerable<Restaurant> GetAll()
         {
             return restaurants;
