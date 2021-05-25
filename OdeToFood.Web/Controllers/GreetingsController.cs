0d8271f1cce1ec03ca4a42b1d4846903dab2251d
DallenPyrah <DallenPyrah@truckstop.com>
2021-05-25 14:51:28 -0600

    Module 2 is done

---
+++ OdeToFood.Web/Controllers/GreetingsController.cs
@@ -0,0 +1,20 @@
+﻿using OdeToFood.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingsController : Controller
+    {
+        // GET: Greetings
+        public ActionResult Index(string name)
+        {
+            var model = new Greetings();
+            model.Name = name ?? "no name";
+            return View(model);
+        }
+    }
+}
